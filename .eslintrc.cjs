be9f958e3420bf223fcd2280315025c08d590c1e
Javier Gutierrez <javierg@airtm.io>
2025-12-26 19:11:50 -0500

    feat: added slint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  plugins: ["react", "react-hooks"],
+  rules: {
+    // React 17+ (no need to import React)
+    "react/react-in-jsx-scope": "off",
+
+    // Common sane defaults
+    "react/prop-types": "off",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+        "newlines-between": "always",
+      },
+    ],
+  },
+};
+ v
